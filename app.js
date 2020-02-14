a67f16917c7d428e87e80a9a386846256494ceb0
Mesay Bekele <me2say@gmail.com>
2020-02-14 16:01:48 -0500

    dark theme

--- app.js
+++ app.js
@@ -14,70 +14,91 @@ const {
 } = require('./lib/index');
 
 let html;
-let cards='';
+let cards = '';
 
-async  function callCordinator () {
-   await testKicker();
+async function callCordinator() {
+    await testKicker();
 
-   await cardForEachRole();
-  
-   html =   htmlGenerator(cards);
-   
-  await  genFile(html)
+    await cardForEachRole();
+
+    html = htmlGenerator(cards);
+
+    await genFile(html)
 
 }
 
 
-async function cardForEachRole(){
+async function cardForEachRole() {
     manager.forEach(mng => {
 
         cards += createCards(mng.getName(), mng.getRole(), mng.getId(), mng.getEmail(), mng.getOfficeNumber())
-    
+
     })
-    
+
     engineer.forEach(eng => {
-    
-        cards+= createCards(eng.getName(), eng.getRole(), eng.getId(), eng.getEmail(), eng.getGithub());
-       
+
+        cards += createCards(eng.getName(), eng.getRole(), eng.getId(), eng.getEmail(), eng.getGithub());
+
     })
 
     intern.forEach(int => {
-    
-        cards += createCards(int.getName(), int.getRole(), int.getId(), int.getEmail(), int.getSchool());
-       
-    })
-    
 
-    
+        cards += createCards(int.getName(), int.getRole(), int.getId(), int.getEmail(), int.getSchool());
+
+    })
+
+
+
 
 }
 
 async function genFile(html) {
     fs.writeFile('./output/index.html', html, function (err) {
-        if(err){
-        console.log('Error Generating file')
+        if (err) {
+            console.log('Error Generating file')
         }
     })
 
 }
 
 function createCards(name, title, id, email, rand) {
+    let uniqueKey;
+    let icon;
+    switch (title) {
+        case 'Manager':
+            uniqueKey = 'Office Number';
+            icon = '<i class="fas fa-user" style="font-size:24px"></i>'
+            break;
+        case 'Engineer':
+            uniqueKey = 'GitHub User Name';
+            icon = '<i class="fas fa-laptop" style="font-size:24px"></i>'
+            break;
+        case 'Intern':
+            uniqueKey = 'School';
+            icon = '<i class="fas fa-graduation-cap" style="font-size:24px"></i>';
+            break;
+    }
+
     let dynamicHTML = ` <div class="col-sm-4 ">
-<div class="card text-white bg-primary mb-3 shadow-lg" style="max-width: 18rem;">
+<div class="card text-white bg-primary mb-3 shadow-lg border rounded-lg" style="max-width: 18rem;">
     <div class="card-header">
         <h5 class="card-title">${name}</h5>
-        <p class="card-title"><i class="fas fa-laptop-code"></i> ${title}</p>
+        <p class="card-title">${icon} ${title}</p>
     </div>
-    <div class="card-body bg-white text-dark">
+    <div class="cardlist">
+    <ul class="list-group bg-white text-dark">
 
-        <div>ID : ${id} </div>
-        <div>Email : ${email} </div>
-        <div>Office Number : ${rand}</div>
+        <li class="list-group-item"><span class="font-weight-bold">ID</span> : ${id} </li>
+        <li class="list-group-item"><span class="font-weight-bold">Email</span> : ${email} </li>
+        <li class="list-group-item"><span class="font-weight-bold">${uniqueKey}</span> : ${rand}</li>
+    </ul>
     </div>
 </div>
 </div>
 `;
-return dynamicHTML
+    return dynamicHTML
 };
 
 callCordinator();
+
+
